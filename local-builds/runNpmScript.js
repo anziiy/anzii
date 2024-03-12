36a90c190152f3216f65d8acd6fc25803db7cf0c
ntsakosurprise <mashelesepru@gmail.com>
2024-03-12 15:58:45 +0200

    chore(root): add publish script for npm publish

--- local-builds/runNpmScript.js
+++ local-builds/runNpmScript.js
@@ -13,8 +13,9 @@ export default function (
 		console.log("command to run", commandToRun);
 		exec(`${commandToRun}`, { cwd: cwd }, (err) => {
 			if (err)
-				console.log(chalk.redBright.bold("Tarball Creation Test Error:"), err);
+				console.log(chalk.redBright.bold("Npm script failed with error:"), err);
 			if (err) reject(false);
+			console.log(chalk.bgGreen.bold("Npm script ran successfully"));
 			resolve(true);
 		});
 	});
