58f162d89f4cbf39e8c29efe90bcc7a1478b8b7b
ntsakosurprise <mashelesepru@gmail.com>
2024-12-01 16:44:41 +0200

    chore(packages/anzii): update options logic

--- packages/anzii/lib/esm/openssl/methods.js
+++ packages/anzii/lib/esm/openssl/methods.js
@@ -22,22 +22,34 @@ export const handleCreateSSLCert = function (data) {
 
 	const loadFile = pao.pa_loadFile;
 	let thisFileDir = getRootDir(__filename);
-	loadFile(path.resolve(thisFileDir, "generateConfig.json")).then(
+	loadFile(path.resolve(thisFileDir, "generateConfig_.json")).then(
 		(sslConfig) => {
 			console.log("SSL CONFIG", sslConfig);
-			console.log("ACTION", JSON.parse(sslConfig).action);
-			let config = JSON.parse(sslConfig);
-			let options = self.runOptions(config);
-			self.runTerminal(`openssl ${options}`);
-			// console.log("RUN TERMINAL OPTIONS", options);
 
-			// self.runTerminal("openssl");
-			// let appConfig = {
-			// 	...config,
-			// 	...apiConfig,
-			// 	router: [...apiConfig.router, ...config.router],
-			// };
-			// resolve(appConfig);
+			let config = JSON.parse(sslConfig);
+
+			// Throw if no config.action or config.options is not defined in the loaded json config
+			if (!config?.action && !config?.actions) {
+				throw new Error(
+					"Openssl config requires config.action or config.actions to be defined",
+				);
+			}
+			let options = "";
+			if (config?.actions) {
+				config.actions.forEach((configItem) => {
+					console.log("THE CONFIG ITEM", configItem);
+					options = self.runOptions(configItem);
+					console.log("Actions options built string", options);
+					self.runTerminal(`openssl ${options}`);
+				});
+				// async.waterfall([self.readHostsFile.bind(self)], (err, result) => {
+				// 	console.log("THE WATERALL RESULTS", result);
+				// 	resolve(result);
+				// });
+			} else {
+				options = self.runOptions(config);
+				self.runTerminal(`openssl ${options}`);
+			}
 		},
 	);
 };
@@ -45,83 +57,70 @@ export const handleCreateSSLCert = function (data) {
 /**
  *
  * @param {*} commandr
- * runTerminal
+ * runTerminal will take a command string that is made up of the openssl parent command
+ *  and its related applicable sub-comands. The sub-commands will have options of their own
  */
-export const runTerminal = function (
-	commandr,
-
-	// scriptPath,
-	// fileToRun,
-	// fromContext,
-	// options,
-	// scriptsPath,
-) {
-	// let optionsValue = options[0][0] ? options[0] : "";
-	// console.log("options value", optionsValue);
-	// console.log("THE SCRIPTS PATH", scriptsPath);
-
-	// let commandToRun = `${commandr} ${path.join(
-	// 	scriptPath,
-	// 	fileToRun,
-	// )} ${optionsValue
-	// 	.toString()
-	// 	.replace(/,/g, " ")} --local-scripts ${scriptsPath}`;
+export const runTerminal = function (commandr) {
 	let commandToRun = `${commandr}`;
-	// console.log("THE PROCESS CWD", process.cwd());
+	console.log("THE COMMAND TO RUN", commandToRun);
 
-	// console.log("COMMAND TO RUN", commandToRun);
 	execSync(`${commandToRun}`, { cwd: process.cwd(), stdio: "inherit" });
 };
 
+/**
+ *
+ * @param {*} config
+ * @returns String
+ *
+ * runOptions is used to process options set for all the different sub-commands of openssl
+ */
 export const runOptions = function (config) {
 	const self = this;
-	console.log(
-		"THE CONFIG",
-		config,
-		config?.action,
-		!config.action || !config.actions,
-	);
-	if (!config?.action && !config?.actions) {
+	console.log("THE COMMANDS", commands);
+
+	// Throw if config action is not by supported by this plugin
+	if (!commands[config.action]) {
 		throw new Error(
-			"Openssl config requires config.action or config.actions to be defined",
+			"Openssl config contains command that is not yet supported",
 		);
 	}
-	if (config?.action) {
-		if (!commands[config.action]) {
-			throw new Error("Openssl config contains that is not yet supported");
-		}
-		let command = commands[config.action];
-		let configActionOptions = config.options;
-		let terminalOptionsFromConfig = "";
-		let commandOptionsKeys = Object.keys(command.options);
-		console.log("THE COMMAND", command);
-		console.log("Command KEYS", commandOptionsKeys);
+	let command = commands[config.action];
+	let configActionOptions = config.options;
+	let terminalOptionsFromConfig = "";
+	let commandOptionsKeys = Object.keys(command.options); // get command options for the current action
+	console.log("THE COMMAND", command);
+	console.log("Command KEYS", commandOptionsKeys);
 
-		commandOptionsKeys.forEach((option) => {
-			console.log("The option", option);
-			console.log("The command ", command.options);
-			console.log("THE COMMNAD.OPTIONS.option", command.options[option]);
-			let commandOption = command.options[option];
-			console.log("THE CONFIG ACTIONS", configActionOptions);
-			if (!configActionOptions[option]) {
+	// Loop through each option
+	commandOptionsKeys.forEach((option) => {
+		console.log("The option", option);
+		console.log("The command ", command.options);
+		console.log("THE COMMNAD.OPTIONS.option", command.options[option]);
+		let commandOption = command.options[option];
+		console.log("THE CONFIG ACTIONS", configActionOptions);
+		if (!configActionOptions[option]) {
+			console.log("THE OPTION IS NOT IN ACTION", option);
+			if (command?.shouldDefault) {
 				if (commandOption?.alias) {
 					terminalOptionsFromConfig += `${commandOption.alias} ${commandOption.value} `;
 				} else {
 					terminalOptionsFromConfig += `${commandOption.value} `;
 				}
-			} else {
-				console.log("THE CONFIG ACTIONS", configActionOptions);
-				if (commandOption?.alias) {
+			}
+		} else {
+			console.log("THE CONFIG ACTIONS", configActionOptions);
+			if (commandOption?.alias) {
+				if (commandOption?.value) {
 					terminalOptionsFromConfig += `${commandOption.alias} ${configActionOptions[option]} `;
 				} else {
-					terminalOptionsFromConfig += `${configActionOptions[option]} `;
+					terminalOptionsFromConfig += `${commandOption.alias} `;
 				}
+			} else {
+				terminalOptionsFromConfig += `${configActionOptions[option]} `;
 			}
-		});
-		console.log("TERMINAL OPTIONS FROM CONFIG", terminalOptionsFromConfig);
+		}
+	});
+	console.log("TERMINAL OPTIONS FROM CONFIG", terminalOptionsFromConfig);
 
-		return `${
-			commands[config.action].commandName
-		} ${terminalOptionsFromConfig}`;
-	}
+	return `${commands[config.action].commandName} ${terminalOptionsFromConfig}`;
 };
