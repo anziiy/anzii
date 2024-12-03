46693194dcc8c9662c866300d9e3e5afd4ee3ba0
ntsakosurprise <mashelesepru@gmail.com>
2024-12-03 04:24:04 +0200

    chore(packages/anzii): enable better generation of certs

--- packages/anzii/lib/esm/openssl/methods.js
+++ packages/anzii/lib/esm/openssl/methods.js
@@ -23,8 +23,8 @@ export const handleCreateSSLCert = function (data) {
 	const loadFile = pao.pa_loadFile;
 	let thisFileDir = getRootDir(__filename);
 	loadFile(path.resolve(thisFileDir, "generateConfig_.json")).then(
-		(sslConfig) => {
-			console.log("SSL CONFIG", sslConfig);
+		async (sslConfig) => {
+			// console.log("SSL CONFIG", sslConfig);
 
 			let config = JSON.parse(sslConfig);
 
@@ -36,19 +36,41 @@ export const handleCreateSSLCert = function (data) {
 			}
 			let options = "";
 			if (config?.actions) {
-				config.actions.forEach((configItem) => {
-					console.log("THE CONFIG ITEM", configItem);
+				let terminalPromises = config.actions.map(async (configItem, loop) => {
+					// console.log("THE CONFIG ITEM", configItem);
 					options = self.runOptions(configItem);
 					console.log("Actions options built string", options);
-					self.runTerminal(`openssl ${options}`);
+					return await runTerminal(`openssl ${options}`, loop);
 				});
+				// console.log("THE GOT FUNKS", gotFunks);
+				// let loop = 0;
+				// let promises = [];
+				// for (let configItem of config.actions) {
+				// 	console.log("THE CONFIG ITEM", configItem);
+				// 	options = self.runOptions(configItem);
+				// 	console.log("Actions options built string", options);
+				// 	loop += 1;
+
+				// 	promises.push(await runTerminal(`openssl ${options}`, loop));
+				// 	// console.log("THE REZOLVED PROMISE", rezolved);
+				// }
+				Promise.all(terminalPromises)
+					.then((opensslActionsResults) => {
+						console.log("The promises have finished", opensslActionsResults);
+					})
+					.catch((err) => {
+						console.log("SSL KEY CREATION FAILED", err);
+					});
+
 				// async.waterfall([self.readHostsFile.bind(self)], (err, result) => {
 				// 	console.log("THE WATERALL RESULTS", result);
 				// 	resolve(result);
 				// });
 			} else {
 				options = self.runOptions(config);
-				self.runTerminal(`openssl ${options}`);
+				self.runTerminal(`openssl ${options}`).then((outcome) => {
+					console.log("SINGLE ACTION OUTCOME", outcome);
+				});
 			}
 		},
 	);
@@ -60,11 +82,14 @@ export const handleCreateSSLCert = function (data) {
  * runTerminal will take a command string that is made up of the openssl parent command
  *  and its related applicable sub-comands. The sub-commands will have options of their own
  */
-export const runTerminal = function (commandr) {
-	let commandToRun = `${commandr}`;
-	console.log("THE COMMAND TO RUN", commandToRun);
-
-	execSync(`${commandToRun}`, { cwd: process.cwd(), stdio: "inherit" });
+export const runTerminal = function (commandToRun, loop) {
+	return new Promise(async (resolve, reject) => {
+		execSync(`${commandToRun}`, {
+			cwd: process.cwd(),
+			stdio: "inherit",
+		});
+		resolve(true);
+	});
 };
 
 /**
@@ -76,7 +101,7 @@ export const runTerminal = function (commandr) {
  */
 export const runOptions = function (config) {
 	const self = this;
-	console.log("THE COMMANDS", commands);
+	// console.log("THE COMMANDS", commands);
 
 	// Throw if config action is not by supported by this plugin
 	if (!commands[config.action]) {
@@ -88,18 +113,18 @@ export const runOptions = function (config) {
 	let configActionOptions = config.options;
 	let terminalOptionsFromConfig = "";
 	let commandOptionsKeys = Object.keys(command.options); // get command options for the current action
-	console.log("THE COMMAND", command);
-	console.log("Command KEYS", commandOptionsKeys);
+	// console.log("THE COMMAND", command);
+	// console.log("Command KEYS", commandOptionsKeys);
 
 	// Loop through each option
 	commandOptionsKeys.forEach((option) => {
-		console.log("The option", option);
-		console.log("The command ", command.options);
-		console.log("THE COMMNAD.OPTIONS.option", command.options[option]);
+		// console.log("The option", option);
+		// console.log("The command ", command.options);
+		// console.log("THE COMMNAD.OPTIONS.option", command.options[option]);
 		let commandOption = command.options[option];
-		console.log("THE CONFIG ACTIONS", configActionOptions);
+		// console.log("THE CONFIG ACTIONS", configActionOptions);
 		if (!configActionOptions[option]) {
-			console.log("THE OPTION IS NOT IN ACTION", option);
+			// console.log("THE OPTION IS NOT IN ACTION", option);
 			if (command?.shouldDefault) {
 				if (commandOption?.alias) {
 					terminalOptionsFromConfig += `${commandOption.alias} ${commandOption.value} `;
@@ -108,10 +133,22 @@ export const runOptions = function (config) {
 				}
 			}
 		} else {
-			console.log("THE CONFIG ACTIONS", configActionOptions);
+			// console.log("THE CONFIG ACTIONS", configActionOptions);
 			if (commandOption?.alias) {
 				if (commandOption?.value) {
-					terminalOptionsFromConfig += `${commandOption.alias} ${configActionOptions[option]} `;
+					if (
+						option == "key-source" ||
+						option == "input-source" ||
+						option == "outputPath"
+					) {
+						if (!path.isAbsolute(option)) {
+							terminalOptionsFromConfig += `${
+								commandOption.alias
+							} ${path.resolve(process.cwd(), configActionOptions[option])} `;
+						}
+					} else {
+						terminalOptionsFromConfig += `${commandOption.alias} ${configActionOptions[option]} `;
+					}
 				} else {
 					terminalOptionsFromConfig += `${commandOption.alias} `;
 				}
@@ -120,7 +157,23 @@ export const runOptions = function (config) {
 			}
 		}
 	});
-	console.log("TERMINAL OPTIONS FROM CONFIG", terminalOptionsFromConfig);
+	// console.log("TERMINAL OPTIONS FROM CONFIG", terminalOptionsFromConfig);
 
 	return `${commands[config.action].commandName} ${terminalOptionsFromConfig}`;
+};
+
+/**
+ *
+ * @param {*} commandr
+ * runTerminal will take a command string that is made up of the openssl parent command
+ *  and its related applicable sub-comands. The sub-commands will have options of their own
+ */
+export const getAsyncFunk = function (commandr) {
+	const self = this;
+	return async () => {
+		self.runTerminal(commandr).then((runOutcom) => {
+			console.log("RUN OUTCOME FOR COMMANDR", commandr, "outcome", runOutcom);
+			resolve(runOutcom);
+		});
+	};
 };
